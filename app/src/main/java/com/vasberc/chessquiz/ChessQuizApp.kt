23981ad4c59b1c932a9784c42ad1f37ba17953cb
vasberc <52175508+vasberc@users.noreply.github.com>
2024-05-16 00:09:16 +0300

    Set up koin (#3)

--- app/src/main/java/com/vasberc/chessquiz/ChessQuizApp.kt
+++ app/src/main/java/com/vasberc/chessquiz/ChessQuizApp.kt
@@ -1,12 +1,28 @@
 package com.vasberc.chessquiz
 
 import android.app.Application
+import com.vasberc.data_local.di.DataLocalModule
+import com.vasberc.presentation.di.PresentationModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.ksp.generated.module
 import timber.log.Timber
 
 class ChessQuizApp: Application() {
     override fun onCreate() {
         super.onCreate()
         initTimber()
+        startKoin()
+    }
+
+    private fun startKoin() {
+        startKoin {
+            androidContext(this@ChessQuizApp)
+            modules(
+                DataLocalModule().module,
+                PresentationModule().module
+            )
+        }
     }
 
     private fun initTimber() {
