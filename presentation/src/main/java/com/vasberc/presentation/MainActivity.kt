fa2b45b0f05775cd08a67fe8a3d061c3526641ec
vasberc <52175508+vasberc@users.noreply.github.com>
2024-05-16 01:55:28 +0300

    Create home screen (#4)

--- presentation/src/main/java/com/vasberc/presentation/MainActivity.kt
+++ presentation/src/main/java/com/vasberc/presentation/MainActivity.kt
@@ -5,12 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.vasberc.presentation.screens.HomeScreen
 import com.vasberc.presentation.ui.theme.ChessQuizTheme
 import timber.log.Timber
 
@@ -21,29 +18,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ChessQuizTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    HomeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ChessQuizTheme {
-        Greeting("Android")
     }
 }
---
+++ presentation/src/main/java/com/vasberc/presentation/navigation/ChessQuizNavHost.kt
@@ -0,0 +1,42 @@
+package com.vasberc.presentation.navigation
+
+import androidx.compose.foundation.background
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.vasberc.presentation.screens.LauncherScreen
+
+@Composable
+fun ChessQuizNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+    onMessage: (message: String) -> Unit
+) {
+
+    NavHost(navController = navController,  startDestination = ChessQuizRoutes.LauncherScreen.route, modifier = modifier) {
+        composable(ChessQuizRoutes.LauncherScreen.route) {
+            LauncherScreen(navController = navController)
+        }
+
+        composable(ChessQuizRoutes.BoardScreen.route, ChessQuizRoutes.BoardScreen.arguments) {
+
+        }
+    }
+}
+
+sealed class ChessQuizRoutes(val route: String) {
+    data object LauncherScreen : ChessQuizRoutes("LauncherScreen")
+    data object BoardScreen : ChessQuizRoutes("BoardScreen?isResume={isResume}") {
+        val arguments = listOf(
+            navArgument("isResume") {
+                type = NavType.BoolType
+                defaultValue = false
+            }
+        )
+    }
+}
