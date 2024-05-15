9e2636b4aceee46a1a28e12ff9f520e0d749fe21
Vasilis Berkovits Ioannidis <vasileios.b@weblicity.com>
2024-05-16 00:05:35 +0300

    data module ok

--- app/src/main/java/com/vasberc/chessquiz/ChessQuizApp.kt
+++ app/src/main/java/com/vasberc/chessquiz/ChessQuizApp.kt
@@ -1,12 +1,26 @@
 package com.vasberc.chessquiz
 
 import android.app.Application
+import com.vasberc.data_local.di.DataLocalModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.ksp.generated.module
 import timber.log.Timber
 
 class ChessQuizApp: Application() {
     override fun onCreate() {
         super.onCreate()
         initTimber()
+        startKoin()
+    }
+
+    private fun startKoin() {
+        startKoin {
+            androidContext(this@ChessQuizApp)
+            modules(
+                DataLocalModule().module
+            )
+        }
     }
 
     private fun initTimber() {
---
+++ data_local/src/main/java/com/vasberc/data_local/di/DataLocalModule.kt
@@ -0,0 +1,8 @@
+package com.vasberc.data_local.di
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+
+@Module
+@ComponentScan("com.vasberc.data_local")
+class DataLocalModule
